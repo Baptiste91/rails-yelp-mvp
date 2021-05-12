4608907f4139ed16e3d375d8594e9760ae177109
baptiste dauphin <bap_dauphin@hotmail.fr>
2021-05-12 14:17:02 +0200

    completed yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+names = ["Chinatown", "Surpriz", "PNY", "Le Ruisseau", "Mamma Roma"]
+address = ["20 rue du plouc, Paris", "5 allée des fleurs, Marseille", "10 rue Aubert, Bordeaux", "Place de la Concorde, Paris", "Avenue des Champs Elysées, Paris"]
+
+5.times do
+  restaurant = Restaurant.new(
+    name: names.sample,
+    address: address.sample,
+    category: categories.sample
+  )
+  restaurant.save!
+end
